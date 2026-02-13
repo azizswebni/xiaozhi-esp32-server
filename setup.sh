d0af71174f22d13c512be9ba31bf0acbb4da9e76
ElGhoulGhazi <ghazielghoul100@gmail.com>
2026-02-13 14:50:10 +0100

    fix_path

--- setup.sh
+++ setup.sh
@@ -29,19 +29,19 @@ else
     echo "curl已安装，跳过安装步骤"
 fi
 
-if [ ! -d /opt/xiaozhi-server/data ]; then
-    mkdir -p /opt/xiaozhi-server/data
-    echo "已创建数据目录: /opt/xiaozhi-server/data"
+if [ ! -d /opt/xiaozhi-esp32-server/data ]; then
+    mkdir -p /opt/xiaozhi-esp32-server/data
+    echo "已创建数据目录: /opt/xiaozhi-esp32-server/data"
 else
-    echo "目录xiaozhi-server/data已存在，跳过创建"
+    echo "目录xiaozhi-esp32-server/data已存在，跳过创建"
 fi
 
 # 检查并创建模型目录
-if [ ! -d /opt/xiaozhi-server/models/SenseVoiceSmall ]; then
-    mkdir -p /opt/xiaozhi-server/models/SenseVoiceSmall
-    echo "已创建模型目录: /opt/xiaozhi-server/models/SenseVoiceSmall"
+if [ ! -d /opt/xiaozhi-esp32-server/models/SenseVoiceSmall ]; then
+    mkdir -p /opt/xiaozhi-esp32-server/models/SenseVoiceSmall
+    echo "已创建模型目录: /opt/xiaozhi-esp32-server/models/SenseVoiceSmall"
 else
-    echo "目录xiaozhi-server/models/SenseVoiceSmall已存在，跳过创建"
+    echo "目录xiaozhi-esp32-server/models/SenseVoiceSmall已存在，跳过创建"
 fi
 
 echo "------------------------------------------------------------"
@@ -51,14 +51,14 @@ sed -i "s|<MQTT_GATEWAY>|'51.38.126.253:1883'|g" ./main/xiaozhi-server/config.ya
 sed -i "s|<MQTT_SIGNATURE_KEY>|'test'|g" ./main/xiaozhi-server/config.yaml 
 sed -i "s|<UDP_GATEWAY>|'51.38.126.253:8884'|g" ./main/xiaozhi-server/config.yaml 
 
-cp ./main/xiaozhi-server/config.yaml /opt/xiaozhi-server/data/.config.yaml
+cp ./main/xiaozhi-server/config.yaml /opt/xiaozhi-esp32-server/data/.config.yaml
 
 
 
 echo "------------------------------------------------------------"
 echo "开始下载语音识别模型"
 # 下载模型文件
-MODEL_PATH="/opt/xiaozhi-server/models/SenseVoiceSmall/model.pt"
+MODEL_PATH="/opt/xiaozhi-esp32-server/models/SenseVoiceSmall/model.pt"
 if [ ! -f "$MODEL_PATH" ]; then
     (
     for i in {1..20}; do
