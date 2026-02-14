c402cf4a9c7445df1283fc8611f5de08dfbe4aca
ElGhoulGhazi <ghazielghoul100@gmail.com>
2026-02-14 14:25:30 +0100

    add_gemini_api_key

--- setup.sh
+++ setup.sh
@@ -50,6 +50,8 @@ echo "Establishing ENV"
 sed -i "s|<MQTT_GATEWAY>|'51.38.126.253:1883'|g" config.yaml 
 sed -i "s|<MQTT_SIGNATURE_KEY>|'AS1531@laksn-dsfq1dsqf3'|g" config.yaml 
 sed -i "s|<UDP_GATEWAY>|'51.38.126.253:8884'|g" config.yaml 
+sed -i "s|<GEMINI_API_KEY>|'AIzaSyDunoemiTpddBAXAAIN_1rS19v_QsBBNnI'|g" config.yaml 
+
 
 cp config.yaml /opt/xiaozhi-esp32-server/data/.config.yaml
 
